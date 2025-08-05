3bbd8aef1fb0ecd27f366bf3bc4528f7a8d53784
mtgsjr <mtgsjr@hotmail.com>
2025-08-05 12:47:43 -0300

    Create setup-owasp-webgoat.sh
    script de configuração do webgoat

---
+++ setup-owasp-webgoat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Conhecida como o Bode da Web
+#setup-owasp-webgoat.sh
+
+echo "O projeto pode ser acessado em https://github.com/WebGoat/WebGoat"
+echo " " -n
+echo "Instale o JDK Java"
+apt install -y openjdk-17-jre
+echo "Este comando fará o download da imagem se ela não existir no Debian e depois executará o container"
+docker run -it -p 127.0.0.1:8080:8080 -p 127.0.0.1:9090:9090 webgoat/webgoat
+
+#Acesse:
+#http://localhost:8080/WebGoat
