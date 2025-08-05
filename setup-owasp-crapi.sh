8911166ce45dca1975afe131a00991fdd6ee3886
mtgsjr <mtgsjr@hotmail.com>
2025-08-05 12:31:22 -0300

    Create setup-owasp-crapi.sh
    Script para preparar o projeto crapi para o treinamento

---
+++ setup-owasp-crapi.sh
@@ -0,0 +1,13 @@
+#setup-owasp-crapi.sh
+
+echo "Download do projeto crAPI da OWASP"
+wget https://github.com/OWASP/crAPI/archive/refs/heads/main.zip
+echo "Descompacte o arquivo e entre na pasta do docker"
+unzip main.zip && cd crAPI-main/deploy/docker
+echo "Fazendo o download da imagem"
+docker-compose pull
+echo "Execute o container"
+docker-compose -f docker-compose.yml --compatibility up -d
+
+#Acesse o projeto em
+#http://localhost:8888
