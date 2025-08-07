7f2c140ed7c36f59afeb375998d682d8b8ffc7dd
mtgsjr <mtgsjr@hotmail.com>
2025-08-07 14:31:45 -0300

    Update atualizacao_docker.sh
    novo docker compose

--- atualizacao_docker.sh
+++ atualizacao_docker.sh
@@ -6,16 +6,16 @@ docker-compose --version
 
 echo -n "Atualizando o Docker e o Docker-Compose..."
 
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian   bookworm stable
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
 
-# atualizando
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin -y
 
-# Mostrar versoes instaladas
-echo "Versoes instaladas"
-docker --version
-docker-compose --version
+sudo usermod -aG docker $USER
+newgrp docker
+docker compose version
+sudo apt autoremove
