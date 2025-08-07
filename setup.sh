773bb4ab44675f0f41481be11aec6aba44552e51
mtgsjr <mtgsjr@hotmail.com>
2025-08-06 22:01:05 -0300

    Update setup.sh
    tira o ~/ do diretorio da toolbox

--- setup.sh
+++ setup.sh
@@ -8,14 +8,14 @@ echo "Atualizações iniciais do Debian"
 sudo apt update && sudo apt upgrade -y
 
 echo "Criando os diretórios da toolbox"
-mkdir -p ~/toolbox-appsec/{recon,fuzzing,scanners,static-analysis,utils,scripts}
+mkdir -p toolbox-appsec/{recon,fuzzing,scanners,static-analysis,utils,scripts}
 
 echo "[+] Instalando utilitários básicos..."
 sudo apt install -y curl wget git unzip vim jq python3 python3-pip build-essential net-tools iputils-ping nmap tmux zsh docker.io docker-compose golang
 
 echo "[+] Ferramenta 1/13 -> Instalando ffuf..."
 go install github.com/ffuf/ffuf/v2@latest
-cp ~/go/bin/ffuf ~/toolbox-appsec/fuzzing/
+cp ~/go/bin/ffuf toolbox-appsec/fuzzing/
 
 echo "[+] Ferramenta 2/13 -> Instalando subfinder..."
 git clone https://github.com/projectdiscovery/subfinder.git
@@ -27,11 +27,11 @@ mv amass toolbox-appsec/recon
 
 echo "[+] Ferramenta 4/13 -> Instalando wfuzz..."
 sudo apt install -y wfuzz
-ln -s $(which wfuzz) ~/toolbox-appsec/fuzzing/wfuzz
+ln -s $(which wfuzz) toolbox-appsec/fuzzing/wfuzz
 
 echo "[+] Ferramenta 5/13 -> Instalando SQLMap..."
 sudo apt install -y sqlmap
-ln -s $(which sqlmap) ~/toolbox-appsec/scanners/sqlmap
+ln -s $(which sqlmap) toolbox-appsec/scanners/sqlmap
 
 echo "[+] Ferramenta 6/13 -> Instalando Nikto..."
 git clone https://github.com/sullo/nikto.git
@@ -43,7 +43,7 @@ mv zaproxy toolbox-appsec/scanners/zap
 
 echo "[+] Ferramenta 8/13 -> Instalando XSStrike..."
 git clone https://github.com/s0md3v/XSStrike ~/toolbox-appsec/scanners/XSStrike
-pip3 install -r ~/toolbox-appsec/scanners/XSStrike/requirements.txt --break-system-packages
+pip3 install -r toolbox-appsec/scanners/XSStrike/requirements.txt --break-system-packages
 
 echo "[+] Ferramenta 9/13 -> Instalando Dirsearch..."
 git clone https://github.com/maurosoria/dirsearch.git
@@ -51,11 +51,11 @@ mv dirsearch toolbox-appsec/fuzzing/
 
 echo "[+] Ferramenta 10/13 -> Instalando Semgrep..."
 pip3 install semgrep --break-system-packages
-ln -s $(which semgrep) ~/toolbox-appsec/static-analysis/semgrep
+ln -s $(which semgrep) toolbox-appsec/static-analysis/semgrep
 
 echo "[+] Ferramenta 11/13 -> Instalando Bandit..."
 pip3 install bandit --break-system-packages
-ln -s $(which bandit) ~/toolbox-appsec/static-analysis/bandit
+ln -s $(which bandit) toolbox-appsec/static-analysis/bandit
 
 echo "[+] Ferramenta 12/13 -> Instalando Gitleaks..."
 git clone https://github.com/gitleaks/gitleaks.git
