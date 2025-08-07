26accf98cf0c1291e1d667079d2e10b5338c0eb9
mtgsjr <mtgsjr@hotmail.com>
2025-08-07 14:36:15 -0300

    Update setup-owasp-crapi.sh
    com docker atualizado

--- setup-owasp-crapi.sh
+++ setup-owasp-crapi.sh
@@ -5,9 +5,9 @@ wget https://github.com/OWASP/crAPI/archive/refs/heads/main.zip
 echo "Descompacte o arquivo e entre na pasta do docker"
 unzip main.zip && cd crAPI-main/deploy/docker
 echo "Fazendo o download da imagem"
-sudo docker-compose pull
+sudo docker compose pull
 echo "Execute o container"
-sudo docker-compose -f docker-compose.yml --compatibility up -d
+sudo docker compose -f docker-compose.yml --compatibility up -d
 
 #Acesse o projeto em
 #http://localhost:8888
