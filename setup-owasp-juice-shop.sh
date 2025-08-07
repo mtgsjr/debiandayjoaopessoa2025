e49e3116b8d71745d4bb36081181312b151baa78
mtgsjr <mtgsjr@hotmail.com>
2025-08-07 12:17:37 -0300

    Update setup-owasp-juice-shop.sh

--- setup-owasp-juice-shop.sh
+++ setup-owasp-juice-shop.sh
@@ -2,9 +2,9 @@
 #OWASP Juice-Shop é provavelmente a mais moderna e sofisticadamente insegura aplicação Web!
 
 echo "Fazendo o download da imagem"
-docker pull bkimminich/juice-shop
+sudo docker pull bkimminich/juice-shop
 echo "Executar o container"
-docker run --rm -p 127.0.0.1:3000:3000 bkimminich/juice-shop
+sudo docker run --rm -p 127.0.0.1:3000:3000 bkimminich/juice-shop
 
 #Abrir em 
 #http://localhost:3000
