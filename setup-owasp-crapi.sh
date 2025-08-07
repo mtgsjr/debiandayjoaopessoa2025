62d0ce96ccce2b74438dbb8de0954a4b29f722c2
mtgsjr <mtgsjr@hotmail.com>
2025-08-07 20:54:49 -0300

    Update setup-owasp-crapi.sh
    sem a atualizacao do docker

--- setup-owasp-crapi.sh
+++ setup-owasp-crapi.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 #setup-owasp-crapi.sh
 
-echo -n "Atualizando o Docker e o Docker-Compose..."
-
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin -y
-
-sudo usermod -aG docker $USER
-newgrp docker
-docker compose version
-sudo apt autoremove
-
 echo "Download do projeto crAPI da OWASP"
 wget https://github.com/OWASP/crAPI/archive/refs/heads/main.zip
 
