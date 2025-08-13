4fb70568b33eb05b7067c67a75c4c6e14f612132
mtgsjr <mtgsjr@hotmail.com>
2025-08-13 20:37:03 -0300

    Update setup-debian-12.11.0-amd64-netinst.sh

--- debian-12.11.0-amd64-netinst/setup-debian-12.11.0-amd64-netinst.sh
+++ debian-12.11.0-amd64-netinst/setup-debian-12.11.0-amd64-netinst.sh
@@ -8,7 +8,7 @@ echo "Atualizações iniciais do Debian"
 sudo apt update && sudo apt upgrade -y
 
 echo "[+] Instalando utilitários básicos..."
-sudo apt install -y curl wget git unzip vim jq python3 python3-pip build-essential net-tools iputils-ping nmap tmux zsh golang apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt install -y curl wget git unzip vim jq python3 python3-pip build-essential net-tools iputils-ping nmap tmux zsh golang apt-transport-https ca-certificates curl gnupg lsb-release docker.io docker-compose
 
 echo "[+] Auto removendo programas obsoletos..."
 sudo apt autoremove
