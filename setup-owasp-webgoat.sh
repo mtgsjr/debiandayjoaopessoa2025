39a7bfbea1552a46555176fcfe91b6ce04146138
mtgsjr <mtgsjr@hotmail.com>
2025-08-15 10:05:10 -0300

    Update setup-owasp-webgoat.sh

--- setup-owasp-webgoat.sh
+++ setup-owasp-webgoat.sh
@@ -4,8 +4,10 @@
 
 echo "O projeto pode ser acessado em https://github.com/WebGoat/WebGoat"
 echo " " 
-echo "Instale o JDK Java"
+
+echo "Instale 12.x.x o JDK Java - para o 13.0.0 não precisa"
 sudo apt install -y openjdk-17-jre
+
 echo "Este comando fará o download da imagem se ela não existir no Debian e depois executará o container"
 sudo docker run -it -p 127.0.0.1:8080:8080 -p 127.0.0.1:9090:9090 webgoat/webgoat
 
